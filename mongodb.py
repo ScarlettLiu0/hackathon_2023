729be6cb5245742e01069a927017a9f0e51670e9
ScarlettLiu0 <55197981+ScarlettLiu0@users.noreply.github.com>
2023-09-28 10:26:32 -0400

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -4,7 +4,7 @@ import json
 
 
 
-uri = "mongodb+srv://scarlett:hackathon2023@cluster1.gmttnh0.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://scarlett:<password>@cluster1.gmttnh0.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 mydb = client["hackathon_2023"]
